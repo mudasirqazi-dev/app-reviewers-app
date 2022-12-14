90c31fa5589436480abc94535ab444e9cdf6786f
mudasirqazi-dev <mudasirqazi.dev@gmail.com>
2022-12-14 13:45:28 +0500

    added prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://app-reviewers-api.vercel.app/api/',
+  imagePath: 'https://app-reviewers-api.vercel.app/api/',
+  login_url: 'https://app-reviewers-api.vercel.app/api/',
 };
