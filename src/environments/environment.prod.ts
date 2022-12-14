b3788df2e6c412af48b1e709af5de6dde81b381c
mudasirqazi-dev <mudasirqazi.dev@gmail.com>
2022-12-15 00:49:44 +0500

    updated environment api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://app-reviewers-apivercel.app/api/',
+  imagePath: 'https://app-reviewers-apivercel.app/api',
+  login_url: 'https://app-reviewers-apivercel.app/api/',
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'https://api.sportsvideolibrary.com/api/',
-  imagePath: 'https://api.sportsvideolibrary.com/api',
-  login_url: 'https://api.sportsvideolibrary.com/api/',
+  api_url: 'https://app-reviewers-apivercel.app/api/',
+  imagePath: 'https://app-reviewers-apivercel.app/api',
+  login_url: 'https://app-reviewers-apivercel.app/api/',
 };
