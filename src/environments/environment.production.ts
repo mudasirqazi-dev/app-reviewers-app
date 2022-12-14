e09e7aecd391375c2719146fa7e7e71de84dad8b
mudasirqazi-dev <mudasirqazi.dev@gmail.com>
2022-12-14 13:41:50 +0500

    fixed url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api_url: 'https://app-reviewers-api.vercel.app/api/',
-  imagePath: 'https://app-reviewers-api.vercel.app/api',
+  imagePath: 'https://app-reviewers-api.vercel.app/api/',
   login_url: 'https://app-reviewers-api.vercel.app/api/',
 };
